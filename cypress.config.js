de64a2cdf81458fd58ac1f244ecdb545c1c3422b
LoredanaVarallo1971 <loredanavarallo71@gmail.com>
2025-05-26 21:07:46 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,13 @@ module.exports = defineConfig({
     baseUrl: 'https://www.globalsqa.com/angularJs-protractor/BankingProject/',    //    per  aprire l'url 
     specPattern: 'cypress/e2e/**/*.cy.js',
     supportFile: false
+    defaultCommandTimeout: 20000,     // wait 20s
+    pageLoadTimeout: 60000,           // max time
+    retries: {
+      runMode: 2,                     //  CI mode, re-try 2 times if fails
+      openMode: 0                     // 
+    },
+    video: true,                      // record a video (utile in CI)
+    screenshotOnRunFailure: true      // screenshot if fails
   }
 })
